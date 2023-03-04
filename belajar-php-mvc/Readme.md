d058573b3ca2fba2482d8f6bd36a43cadec31a11
mhdryan09 <pranataryan91@gmail.com>
2023-03-05 02:19:11 +0700

    Materi : Routing

--- belajar-php-mvc/Readme.md
+++ belajar-php-mvc/Readme.md
@@ -5,3 +5,5 @@ app : folder yang akan menyimpan folder dan file utama dari aplikasi MVC kita.
 core : folder utama yang berisi file pembentuk MVC
 
 controllers : folder yang berisi file controller yang extends ke class Controller di folder core
+
+file .htaccess adalah file yang bisa kita gunakan untuk memodifikasi konfigurasi dari server apache kita dan itu bisa lakukan per folder
--- belajar-php-mvc/app/core/App.php
+++ belajar-php-mvc/app/core/App.php
@@ -4,6 +4,26 @@ class App
 {
   public function __construct()
   {
-    echo "Hello World";
+    // jalankan fungsi parseURL
+    $url = $this->parseURL();
+    var_dump($url);
+  }
+
+  // fungsi untuk mengecek URL
+  public function parseURL()
+  {
+    // jika ada URL yang dikirimkan
+    if (isset($_GET['url'])) {
+      // isi variabel url dgn url yang kita kirimkan di halaman
+      $url = rtrim($_GET['url'], '/');
+      // rtrim artinya membersihkan dari suatu character tertentu
+
+      // bersihkan url dari inputan user seperti character aneh
+      $url = filter_var($url, FILTER_SANITIZE_URL);
+
+      // kemudian kita pecah url menjadi elemen array
+      $url = explode('/', $url);
+      return $url;
+    }
   }
 }
